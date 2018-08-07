35b4d105585f02e23909d876df464576b40e904d
Sebastien Ong Tone <s.ongtone@hotmail.com>
2018-08-06 20:17:06 -0400

    start of the project, made modification to the default settings

---
+++ KusteezFormApp/Models/FormDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KusteezFormApp.Models
+{
+    public class FormDetails
+    {
+        public string gamerTag { get; set; }
+
+
+        public FormDetails()
+        {
+            this.gamerTag = string.Empty;
+        }
+
+    }
+
+    
+}
