529cf9a4d0045d17e1a1d69dae1f325e1690489e
Sebastien Ong Tone <s.ongtone@hotmail.com>
2018-08-08 23:27:12 -0400

    Added connection to database

--- KusteezFormApp/Controllers/HomeController.cs
+++ KusteezFormApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using KusteezFormApp.Models;
+using MySql.Data.MySqlClient;
 
 namespace KusteezFormApp.Controllers
 {
@@ -12,12 +13,38 @@ namespace KusteezFormApp.Controllers
     {
         public IActionResult Index()
         {
+            FormDetails fd = new FormDetails();
+            //This is my connection string i have assigned the database file address path  
+            string sql = "server=localhost;user id=root;password=1234;database=testing";
+            MySqlConnection conn = new MySqlConnection(sql);
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select name from testing where idtesting = 1";
+
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                return View();
+            }
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                fd.clothingType = reader["name"].ToString();
+            }
+
+            conn.Close();
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+           
+                
+             
 
             return View();
         }
